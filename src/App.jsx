35a6c0135da9d0264c89e8a6126440daf6d02f73
humaidshaykh <humaidshaykh@gmail.com>
2024-09-02 01:50:09 +0500

    Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
+import { useState } from 'react';
 import './App.css'
 
-function App() {
+function taskForm() {
+
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Task Title:', title);
+    console.log('Task Description:', description);
+
+    setTitle('');
+    setDescription('');
+  };
 
   return (
     <>
+
       <div>
-        <h1 className='heading'>Hello World</h1>
+        <form className='parentDiv' onSubmit={handleSubmit}>
+          <input type="text" placeholder='Enter Task Title' value={title} onChange={handleTitleChange} className='inputField' required />
+          <textarea placeholder='Enter Task Description' value={description} onChange={handleDescriptionChange} className='textArea' required></textarea>
+          <button type='submit' className='submitButton'>Add Task</button>
+        </form>
       </div>
+
     </>
-  )
+  );
 }
 
-export default App
+export default taskForm
